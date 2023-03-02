ace2e5fd5d926ebe8fbb6200e75ba9174c65e7c1
Daniel Papp <79407173+Silzara@users.noreply.github.com>
2023-03-02 15:50:38 +0100

    day1 update

--- src/main/java/com/example/bildorganisator_ihk_projekt/Browse.java
+++ src/main/java/com/example/bildorganisator_ihk_projekt/Browse.java
@@ -1,9 +1,7 @@
 package com.example.bildorganisator_ihk_projekt;
 
 import javax.swing.*;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Stack;
+
 
 public class Browse {
     /**
