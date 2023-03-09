7cc252200c61292a7482bccb7587e1b0f74a0552
Daniel Papp <79407173+Silzara@users.noreply.github.com>
2023-03-09 14:04:50 +0100

    week1 update 1

--- src/main/java/com/example/bildorganisator_ihk_projekt/RequestLink.java
+++ src/main/java/com/example/bildorganisator_ihk_projekt/RequestLink.java
@@ -15,43 +15,52 @@ public class RequestLink {
     public RequestLink() {
     }
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 //        String a ="51.514244";
 //        String b ="7.468429";
 //        getLocation(a,b);
     }
 
+    /**
+     * Retrieves the location details for the given latitude and longitude using the OpenStreetMap Nominatim API.
+     * <p>
+     * The API returns the XML response which is then parsed to extract the city, state and country information.
+     *
+     * @param latitude  The latitude coordinate for the location.
+     * @param longitude The longitude coordinate for the location.
+     * @throws IOException If an I/O error occurs while reading from the URL connection.
+     */
     public static void getLocation(String latitude, String longitude) throws IOException {
-        //Instantiating the URL class
+//Instantiating the URL class
         URL url = new URL("https://nominatim.openstreetmap.org/reverse?format=xml&" +
                 "lat=" + latitude +
                 "&lon=" + longitude
                 + "&zoom=8&addressdetails=1");
-        //Retrieving the contents of the specified page
+
+//Retrieving the contents of the specified page
         Scanner gl = new Scanner(url.openStream());
-        //Instantiating the StringBuffer class to hold the result
+
+//Instantiating the StringBuilder class to hold the result
         StringBuilder dp = new StringBuilder();
         while (gl.hasNext()) {
             dp.append(gl.next());
-            //System.out.println(sc.next());
         }
-        //Retrieving the String from the String Buffer object
-        String result = dp.toString();
-        //System.out.println(result);
-        //Removing the HTML tags
-        //result = result.replaceAll("<[^>]*>", "");
-        //System.out.println("Contents of the web page: " + result);
-        Pattern city = Pattern.compile("<city>(.*?)</city");
-        Pattern state = Pattern.compile("<state>(.*?)</state>");
-        Pattern country = Pattern.compile("<country>(.*?)</country>");
-        Matcher cityMatcher = city.matcher(result);
-        if(cityMatcher.find()) System.out.println(cityMatcher.group(1));
-        Matcher stateMatcher = state.matcher(result);
-        if(stateMatcher.find()) System.out.println(stateMatcher.group(1));
-        Matcher countryMatcher = country.matcher(result);
-        if(countryMatcher.find()) System.out.println(countryMatcher.group(1));
 
-       // System.out.println(city);
+//Retrieving the String from the StringBuilder object
+        String result = dp.toString();
+
+//Extracting city, state and country information from the XML response
+        Pattern city = Pattern.compile("<city>(.*?)</city");
+        Matcher cityMatcher = city.matcher(result);
+        if (cityMatcher.find()) System.out.println(cityMatcher.group(1));
+
+        Pattern state = Pattern.compile("<state>(.*?)</state>");
+        Matcher stateMatcher = state.matcher(result);
+        if (stateMatcher.find()) System.out.println(stateMatcher.group(1));
+
+        Pattern country = Pattern.compile("<country>(.*?)</country>");
+        Matcher countryMatcher = country.matcher(result);
+        if (countryMatcher.find()) System.out.println(countryMatcher.group(1));
     }
 
 }
--- src/main/java/com/example/bildorganisator_ihk_projekt/test.java
+++ src/main/java/com/example/bildorganisator_ihk_projekt/test.java
@@ -8,9 +8,9 @@ public class test {
     public static void main(String[] args) throws IOException {
         new RequestLink();
         String longitude ="51.514244"; // N
-        // if the coordinates are S, add - before the longitude number
+        // if the longitude coordinates are S, add - before the number
         String latitude ="7.468429";  // E
-        // if the coordinates are W, add - before the latitude number
+        // if the latitude coordinates are W, add - before the number
         getLocation(longitude,latitude);
     }
 }
